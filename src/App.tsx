dc2d89bfc7d03719cdf28a2017175fb747838082
FukenMike <mikegriggs929@gmail.com>
2025-05-15 13:54:15 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import WhoWeAre from './pages/WhoWeAre';
-import Mission from './pages/Mission';
-import Vision from './pages/Vision';
-import Donate from './pages/Donate';
-import FounderStory from './pages/FounderStory';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/who-we-are' element={<WhoWeAre />} />
-        <Route path='/mission' element={<Mission />} />
-        <Route path='/vision' element={<Vision />} />
-        <Route path='/donate' element={<Donate />} />
-        <Route path='/founder-story' element={<FounderStory />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
